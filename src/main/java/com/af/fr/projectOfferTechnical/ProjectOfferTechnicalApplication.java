4bf5e2c7d250fccc4527d06f8b613728e21472ed
clement.escande@3wa.io <clement.escande@3wa.io>
2022-11-02 09:27:57 +0100

    Installation du projet 1.0.0

---
+++ src/main/java/com/af/fr/projectOfferTechnical/ProjectOfferTechnicalApplication.java
@@ -0,0 +1,13 @@
+package com.af.fr.projectOfferTechnical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOfferTechnicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOfferTechnicalApplication.class, args);
+	}
+
+}
